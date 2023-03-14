3a8b15bf1d424432f41c803fcb7a34c65338c9cc
kumarnivi <nivethikashivakumar56@gmail.com>
2023-03-14 08:56:47 +0530

    version02

---
+++ script.js
@@ -0,0 +1,23 @@
+function setTime() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const minutes = now.getMinutes();
+	const hours = now.getHours();
+
+	const secondHand = document.querySelector('.second-hand');
+	const minuteHand = document.querySelector('.minute-hand');
+	const hourHand = document.querySelector('.hour-hand');
+
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+	const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	const digitalClock = document.querySelector('.digital-clock');
+	digitalClock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(setTime, 1000);
